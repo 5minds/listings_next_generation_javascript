f1b21ca947e7eb5172989b8048d338e523add48e
Martin Moellenbeck <martin.moellenbeck@5minds.de>
2014-05-02 14:14:48 +0200

    added simple samples for arrow functions

---
+++ 02_arrow_functions/01_sample.js
@@ -0,0 +1,42 @@
+// bad sample
+var badObj = {
+	name: "Holger",
+
+	handleMessage: function(msg, callback) {
+		callback(msg);
+	},
+
+	doIt: function() {
+		var that = this;
+
+		this.handleMessage("Hi ", function(msg) {
+			console.log(msg + that.name);
+		});
+	}
+};
+
+badObj.doIt();
+
+
+// good sample
+var goodObj = {
+	name: "Martin",
+
+	handleMessage: function(msg, callback) {
+		callback(msg);
+	},
+
+	doIt: function() {
+		// lange Schreibweise
+		this.handleMessage("Guten Tag ", (msg) => {
+			console.log(msg + this.name);
+		});
+
+		// kurze Schreibweise
+		this.handleMessage("Hi ", msg => console.log(msg + this.name));
+	}
+
+};
+
+goodObj.doIt();
+;
